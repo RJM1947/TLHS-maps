4ec829687b547082f6536d8a0734e4bb435b1282
RJM1947 <71822331+RJM1947@users.noreply.github.com>
2020-09-24 11:47:19 +0100

    Add files via upload

---
+++ 49Features.js
@@ -0,0 +1,3 @@
+{'type': 'Feature', 'geometry': {'type' : 'Point' , 'coordinates' : [0.346759, 51.584441]}, 'properties': {'name': 'OLD HALL POND'} };
+
+
---
+++ sleepers.js
@@ -0,0 +1,8 @@
+var sleepers = {"type": "FeatureCollection", 
+                "features": 
+                    [{"type": "Feature","properties": {"name": "Sleepers Farm","popupContent": "<img src=https://www.thurrock-history.org.uk/pictures/sleepers.jpg width=300>Sleepers Farm<a href=https://www.thurrock-history.org.uk/sleepers.html> more information …</a>"},"geometry": {"type": "Point", "coordinates":[0.368533, 51.481517]}},
+                    {"type": "Feature","properties": {"name": "another name","popupContent": "<img src=https://www.thurrock-history.org.uk/pictures/sleepers.jpg width=300>Sleepers Farm<a href=https://www.thurrock-history.org.uk/sleepers.html> more information …</a>"},"geometry": {"type": "Point", "coordinates":[0.358533, 51.491517]}},
+                    {"type": "Feature","properties": {"name": "another name","popupContent": "<img src=https://www.thurrock-history.org.uk/pictures/sleepers.jpg width=300>Sleepers Farm<a href=https://www.thurrock-history.org.uk/sleepers.html> more information …</a>"},"geometry": {"type": "Point", "coordinates":[0.363629, 51.515071]}}
+                    ]
+                };
+
